653c3f79d0fc1edaf3a7c1b1ced93abff61095ff
LachlanStephan <ljstephan116@gmail.com>
2020-11-17 13:50:07 +1000

    updated the render, still no display

--- marvel-api/src/components/pages/Comics.js
+++ marvel-api/src/components/pages/Comics.js
@@ -8,29 +8,40 @@ export default class Comics extends React.Component {
     };
 
     async componentDidMount() {
-        const url = "http://gateway.marvel.com/v1/public/comics?ts=1&apikey=f0b6fb5f90e9139ed2f1514d0" +
-                "139fb15&hash=ccb8f319be84ea5586be53927142ff35";
+        const url = "http://gateway.marvel.com/v1/public/comics?&apikey=f0b6fb5f90e9139ed2f1514d0139fb" +
+                "15&ts=1&hash=ccb8f319be84ea5586be53927142ff35";
         const response = await fetch(url);
         const data = await response.json();
         console.log(data);
-        this.setState({ thumbnail: data.results, loading: false });
+        this.setState({comics: data.results, loading: false});
     }
 
     render() {
+        if (this.state.loading) {
+            return <Container>
+                <Row>loading...</Row>
+            </Container>
+        }
+
+        if (!this.state.comics) {
+            return <Container>
+                <Row>Didn't get anything</Row>
+            </Container>
+        }
+
         return (
             <Container>
+       
+                    {this.state.comics.map(comics => (
                 <Row>
-                    <Col>
-                        {this.state.loading || !this.state.thumbnail
-                            ? (
-                                <Col>Loading...</Col>
-                            )
-                            : (
-                                <Col>this.state.data.results.thumbnail</Col>
-                            )}
-                    </Col>
+                    <Col>{comics}</Col>
+                    <Col>{comics}</Col>
+                    <Col>{comics}</Col>
+                    <Col>{comics}</Col>
+       
                 </Row>
+            ))}
             </Container>
-        )
+        );
     }
-};
+}
